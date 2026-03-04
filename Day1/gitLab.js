8405b59ef7652c57b31eea3d5211f5efd93f16d1
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-03-04 11:22:04 +0200

    added method: printHelloITI

--- Day1/gitLab.js
+++ Day1/gitLab.js
@@ -1,5 +1,10 @@
 class HelloWorld {
+
     printHelloWorld() {
-        console.log("Hello World")
+        console.log("Hello World");
+    }
+
+    printHellowITI() {
+        console.log("Hello ITI");
     }
 }
